074d772ccb2cc5e45bf880a6130c1585007995b7
Vedant Paranjape <vedantparanjape160201@gmail.com>
2019-06-24 23:06:22 +0530

    major changes

--- src/network_manager.cpp
+++ src/network_manager.cpp
@@ -2,12 +2,12 @@
 
 using namespace boost::asio;
 
-sock network_manage::client_init(std::string ip, int port)
+std::shared_ptr<ip::tcp::socket> network_manage::client_init(std::string ip, int port)
 {
     //// add error checking 
     //// make sure only one of the following gets's called
     ep = ip::tcp::endpoint(ip::address::from_string(ip), port);
-    sock sckt(service);
+    std::shared_ptr<ip::tcp::socket> sckt(new ip::tcp::socket(service));
     sckt->connect(ep);
 
     return sckt;
@@ -20,10 +20,10 @@ int network_manage::server_init(std::string ip, int port)
     acc = ip::tcp::acceptor(service, ep);
 }
 
-sock network_manage::accept_client()
+std::shared_ptr<ip::tcp::socket> network_manage::accept_client()
 {
     // do this only if init as a server
-    sock sckt(service);
+    std::shared_ptr<ip::tcp::socket> sckt(new ip::tcp::socket(service));
     acc.accept(*sckt);
 
     return sckt;
@@ -32,4 +32,16 @@ sock network_manage::accept_client()
 network_manage::~network_manage()
 {
     service.stop();
+}
+
+int main()
+{
+    network_manage nm;
+
+    nm.server_init("127.0.0.1", 3434);
+    std::shared_ptr<ip::tcp::socket> sc = nm.accept_client();
+
+    sc->write_some(boost::asio::buffer("hello world !!", 14));
+
+    sc->close();
 }
--- src/network_manager.hpp
+++ src/network_manager.hpp
@@ -7,13 +7,12 @@ class network_manage{
       io_service service;
       ip::tcp::endpoint ep;
       ip::tcp::acceptor acc;
-      typedef std::shared_ptr<ip::tcp::socket> sock;
       bool isServer = false;
 
     public:
       network_manage();
-      sock client_init(std::string ip, int port);
+      std::shared_ptr<ip::tcp::socket> client_init(std::string ip, int port);
       int server_init(std::string, int port);
-      sock accept_client();
+      std::shared_ptr<ip::tcp::socket> accept_client();
       ~network_manage();
 };
